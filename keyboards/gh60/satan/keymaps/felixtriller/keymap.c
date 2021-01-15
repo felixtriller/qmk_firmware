b85d875c48b11998dba40adc0c0adc49ebab08c0
Felix Triller <ftriller@cisco.com>
2021-01-15 09:47:33 +0100

    KC_DEL.

--- keyboards/gh60/satan/keymaps/felixtriller/keymap.c
+++ keyboards/gh60/satan/keymaps/felixtriller/keymap.c
@@ -15,10 +15,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_FN] = LAYOUT_60_ansi(
-    KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   RESET,  \
+    KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,   \
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PGUP,  _______,  _______,  _______,  BL_DEC,   BL_INC,   BL_TOGG,  \
     KC_LCTL,  _______,  _______,  KC_PGDN,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, _______,  _______,            _______,  \
-    _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  \
+    _______,            _______,  _______,  _______,  _______,  BL_STEP,  _______,  _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,            _______,  \
     KC_LCTL,  KC_LALT,  KC_LGUI,                                KC_SPC,                                 _______,  KC_RGUI,  KC_RALT,  KC_RCTL
   )
 };
